93a3bcd7cac04c8d06efc85d9b12abfbd024d3f6
F.S. Nobels <nobels@virgo03.intra.astro.rug.nl>
2016-12-23 13:42:57 +0100

    adding datalists

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python
 from __future__ import division
 
-print 'Hello world!'
+import numpy as np
+
+datatest = np.loadtxt('test.txt')
+datatraining = np.loadtxt('training.txt')
+
+for i in xrange(0,len(datatraining)):
+     print '%6d.jpg'%datatest[i]
+
+     print 
