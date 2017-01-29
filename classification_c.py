45a97302d7849fe8541f948f780213fd0a852c4c
Bas Roelenga <basroelenga1@gmail.com>
2017-01-29 11:55:14 +0100

    Classification

--- classification_c.py
+++ classification_c.py
@@ -56,18 +56,19 @@ layernumb = int(sys.argv[1])
 print("Number of neurons in hidden layer: " ,layernumb)
 
 # Use the machine learning from sci-kit learn.
-mlp = MLPClassifier(hidden_layer_sizes=(layernumb), activation="tanh", solver="lbfgs", max_iter=1000)
+mlp = MLPClassifier(hidden_layer_sizes=(layernumb), activation="tanh", max_iter=1000)
 
 # Fit the data.
 X_train, X_test, y_train, y_test = train_test_split(data_list,
                                                     clas_list,
                                                     test_size=0.25)
 
-#mlp.fit(X_train, y_train)
+mlp.fit(X_train, y_train)
 
 # Test the model by predicting on the rest of the test set.
-#print ('score', mlp.score(X_test,y_test))
+print ('score', mlp.score(X_test,y_test))
 
+'''
 # Test for best number of hidden layers
 neurons = np.linspace(10, 400, 40)
 score_list = []
@@ -88,6 +89,71 @@ plt.plot(neurons, score_list)
 
 plt.xlabel("neurons")
 plt.ylabel("score")
+'''
+t2 = time.time()
+print ('total time = %1.4f seconds' %(t2-t1))
+
+print(np.shape(image_data))
+
+# Starting deep convelution
+import theano
+import lasagne
+from lasagne import layers
+
+from lasagne.updates import nesterov_momentum
+from nolearn.lasagne import NeuralNet
+from nolearn.lasagne import visualize
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+
+net1 = NeuralNet(
+    layers=[('input', layers.InputLayer),
+            ('conv2d1', layers.Conv2DLayer),
+            ('maxpool1', layers.MaxPool2DLayer),
+            ('conv2d2', layers.Conv2DLayer),
+            ('maxpool2', layers.MaxPool2DLayer),
+            ('dropout1', layers.DropoutLayer),
+            ('dense', layers.DenseLayer),
+            ('dropout2', layers.DropoutLayer),
+            ('output', layers.DenseLayer),
+            ],
+    # input layer
+    input_shape=np.shape((750, 50, 50, 3)),
+    # layer conv2d1
+    conv2d1_num_filters=32,
+    conv2d1_filter_size=(5, 5),
+    conv2d1_nonlinearity=lasagne.nonlinearities.rectify,
+    conv2d1_W=lasagne.init.GlorotUniform(),  
+    # layer maxpool1
+    maxpool1_pool_size=(2, 2),    
+    # layer conv2d2
+    conv2d2_num_filters=32,
+    conv2d2_filter_size=(5, 5),
+    conv2d2_nonlinearity=lasagne.nonlinearities.rectify,
+    # layer maxpool2
+    maxpool2_pool_size=(2, 2),
+    # dropout1
+    dropout1_p=0.5,    
+    # dense
+    dense_num_units=256,
+    dense_nonlinearity=lasagne.nonlinearities.rectify,    
+    # dropout2
+    dropout2_p=0.5,    
+    # output
+    output_nonlinearity=lasagne.nonlinearities.softmax,
+    output_num_units=10,
+    # optimization method params
+    update=nesterov_momentum,
+    update_learning_rate=0.01,
+    update_momentum=0.9,
+    max_epochs=10,
+    verbose=1,
+    )
+
+train_x = np.reshape(X_train, (len(X_train), 50, 50, 3))
+print(np.shape(train_x))
+nn = net1.fit(train_x, y_train)
+
 
 t2 = time.time()
 print ('total time = %1.4f seconds' %(t2-t1))
